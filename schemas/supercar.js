9823d3274ec0db3b35848f90ec0819e0def97336
nack <jirapong.k@touchtechnologies.co.th>
2022-11-17 17:44:26 +0700

    fal

---
+++ schemas/supercar.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'supercars',
+  type: 'document',
+  title: 'Supercars',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'detail',
+      type: 'string',
+      title: 'Detail',
+    },
+    {
+      name: 'images',
+      type: 'image',
+      title: 'Images',
+    },
+  ],
+}
